b735bdfced21bfb0ded1e9af8b3a723531ab7f9e
kormonditamas2222 <t.kormond2006@gmail.com>
2025-02-14 11:03:30 +0100

    class kész, fájl beolvasva

---
+++ karakterek/Karakter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace karakterek
+{
+	internal class Karakter
+	{
+		private string nev;
+		private int szint;
+		private int eletero;
+		private int ero;
+
+		public Karakter(string nev, int szint, int eletero, int ero)
+		{
+			this.nev = nev;
+			this.szint = szint;
+			this.eletero = eletero;
+			this.ero = ero;
+		}
+
+		public string Nev { get => nev; set => nev = value; }
+		public int Szint { get => szint; set => szint = value; }
+		public int Eletero { get => eletero; set => eletero = value; }
+		public int Ero { get => ero; set => ero = value; }
+
+		public override string ToString()
+		{
+			return $"{this.nev} {this.szint} {this.eletero} {this.ero}";
+		}
+	}
+}
